8455a92aaabdb31f815174ffc5e7e02e3d3fad97
The Coder <82015917+thecoderlive@users.noreply.github.com>
2022-10-05 05:01:06 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,145 @@
+export const item = {
+    "id": "696eb532-619c-43f1-8e7e-56f17ee28580",
+    "home_feed": [
+        {
+            "id": "cbbc4ac6-1ffa-4b07-be09-8df30bcf434b",
+            "artist_name": "man",
+            "home_carousel": [
+                {
+                    "id": "571a5650-f395-431a-b8ab-9b8404902d05",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/344.png"
+                },
+                {
+                    "id": "a1eaa5b4-7721-46a5-af0d-15ec43b5a850",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/229.png"
+                },
+                {
+                    "id": "201ca625-2a93-4c07-8f89-750424c5fe28",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/235.png"
+                },
+                {
+                    "id": "c63e9559-67bf-4f7e-b59e-a520aee0a47b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/156.png"
+                },
+                {
+                    "id": "4d48a6b7-d7b8-4b73-a3f3-6f4b1c4d328e",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/259.png"
+                }
+            ],
+            "art_caption": "cut",
+            "art_description": "Bar beautiful enter hope have. Significant nearly his."
+        },
+        {
+            "id": "d10893cf-98e4-4d44-96fd-0efcaa45baae",
+            "artist_name": "official",
+            "home_carousel": [
+                {
+                    "id": "4870ebce-68b1-4fa9-9375-4526c29a5ea3",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/92.png"
+                },
+                {
+                    "id": "df2faa44-e4dc-4280-9ee8-17603ab4112d",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/305.png"
+                },
+                {
+                    "id": "32427607-0f65-4c44-9705-79fda3ea89be",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/272.png"
+                },
+                {
+                    "id": "a520d73e-fb49-4e69-ba04-b94d099d029d",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/102.png"
+                },
+                {
+                    "id": "e6c17da5-1c4e-4caf-b314-9c6b8a4fe73a",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/321.png"
+                }
+            ],
+            "art_caption": "play",
+            "art_description": "Something simply talk trouble use process per."
+        },
+        {
+            "id": "26fbb854-2cba-41a3-842b-bac672014522",
+            "artist_name": "discuss",
+            "home_carousel": [
+                {
+                    "id": "1062d705-acc0-424f-a732-0df47ed10c9e",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/244.png"
+                },
+                {
+                    "id": "740c89a9-5411-452c-9a3b-b28e241ac25e",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/11.png"
+                },
+                {
+                    "id": "7f6d4159-1762-47e7-97f9-768254b7a7d6",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/67.png"
+                },
+                {
+                    "id": "3cae6e5f-79c4-46df-bf7a-bee2dc9d672a",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/189.png"
+                },
+                {
+                    "id": "6f96f2bc-2bab-4e48-a1a1-5885ecc26baf",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/186.png"
+                }
+            ],
+            "art_caption": "reveal",
+            "art_description": "Continue stand month sense matter difference address create. Home while traditional show career body laugh on. His throw election save."
+        },
+        {
+            "id": "65065d23-84ca-46b4-8b07-a29eb7f3a2b6",
+            "artist_name": "others",
+            "home_carousel": [
+                {
+                    "id": "700cc74b-eec4-4854-b9fc-a3c862b0e1d3",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/290.png"
+                },
+                {
+                    "id": "3a7b5142-bdce-4500-876c-da9dffda434e",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/123.png"
+                },
+                {
+                    "id": "f898e618-2633-4b03-a58c-b6aaf815a872",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/151.png"
+                },
+                {
+                    "id": "d79ae7fd-65a7-422f-91e2-26bc9f320fb1",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/221.png"
+                },
+                {
+                    "id": "0b253a01-41df-48c6-bb72-cb3f89051989",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/309.png"
+                }
+            ],
+            "art_caption": "follow",
+            "art_description": "Summer sell he commercial over picture. At easy happy. Include situation memory across like. Both head attorney."
+        },
+        {
+            "id": "5f97369a-13ff-44d4-86de-833d45c0cdbb",
+            "artist_name": "report",
+            "home_carousel": [
+                {
+                    "id": "a2076c84-91bc-4cdf-ac64-316e7e071eaa",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/247.png"
+                },
+                {
+                    "id": "1b63a011-c202-4de9-8c74-2005a02fcfd7",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/30.png"
+                },
+                {
+                    "id": "7bbdcc80-c9d6-40a9-99c6-a9dd88eac3fc",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/68.png"
+                },
+                {
+                    "id": "624fc906-e90d-4fc6-805f-32d7a3b202cf",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/94.png"
+                },
+                {
+                    "id": "3bcd7ba4-82fc-41fa-825d-8112abe1529e",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/361.png"
+                }
+            ],
+            "art_caption": "total",
+            "art_description": "News lose no think. Live suffer determine parent program seek push. Wind fill similar college. Traditional exactly color those building decision call bad."
+        }
+    ]
+}
